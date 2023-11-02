0d146e7cb3e064b23d7eca1e853b963db23e8537
ArcaLunar <139364943+ArcaLunar@users.noreply.github.com>
2023-11-02 13:58:24 +0800

    Update expression_calc.h

--- expression_calc.h
+++ expression_calc.h
@@ -33,7 +33,28 @@ long double parseDouble(char a[]) {
 long double getNextDouble(char a[], int* cur) {
     int index = 0;
     int i;
-    char target[];
+    char target[MAXN];
+    clearArray(target, MAXN);
+    for(i = *cur; i < strlen(a); i++) {
+        if(a[i] == ' ') continue;
+        if(!(a[i] >= '0' && a[i] <= '9' || a[i] == '.')) break;
+        target[index++] = a[i];
+    }
+    *cur = i;
+    return parseDouble(target);
+}
+
+long double calculate(struct dataStack *a) {
+    struct dataStack tmp;
+    initData(&tmp);
+    for(int i = 0; i < a->top; i++) {
+        if(a->aux[i] == 1) pushData(&tmp, a->data[i]);
+        else {
+            long double a1 = popData(&tmp);
+            long double a2 = popData(&tmp);
+            pushData(&tmp, 
+        }
+    }
 }
 
 #endif
